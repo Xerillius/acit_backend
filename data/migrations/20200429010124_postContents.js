06068c5ead7825bb13864f6ed8498a5ffa9b6fba
Christopher Quinn <caquinn.86@gmail.com>
2020-04-29 01:33:51 -0700

    created migration for tracking blogpost content

--- data/migrations/20200429010124_postContents.js
+++ data/migrations/20200429010124_postContents.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('post_contents', table => {
+    table
+      .increments();
+    table
+      .integer('blogpost_id');
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('post_contents');
 };
