8e193c8dea45163c0cfd8e290aed2b304b835c96
Christopher Quinn <caquinn.86@gmail.com>
2020-05-06 05:44:18 -0700

    fixed database url processing

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: process.env.DB_URL + '?ssl=true',
+    connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
       max: 10
